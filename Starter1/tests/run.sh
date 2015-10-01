9d3443306095edcb18ad24a3956a970133df223f
Ryan Kang <yimingkang@hotmail.com>
2015-10-01 17:22:47 -0400

    Improve test && add 'make test' to Makefile

--- Starter1/tests/run.sh
+++ Starter1/tests/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 for file in ./*.glsl; do
     ../compiler467 -Tn "$file" > "$file".out
-    diff "$file".out "$file".target
+    DIFF=$(diff "$file".out "$file".target)
+    if [ "$DIFF" != "" ] 
+    then
+        echo "*****TEST $file FAILED*****"
+        diff "$file".out "$file".target
+    else
+        echo "OK"
+    fi
 done
