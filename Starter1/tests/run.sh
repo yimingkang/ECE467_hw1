18cc4e2ad80663eb86d84b0f4f50b60817e4731a
tabletenniser <zexuan.wang@mail.utoronto.ca>
2015-10-01 18:11:08 -0400

    Update testing files

--- Starter1/tests/run.sh
+++ Starter1/tests/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "=================== TESTING STARTS ===================="
+echo ""
 for file in ./*.glsl; do
     ../compiler467 -Tn "$file" > "$file".out
     diff "$file".out "$file".target
 done
+echo "================+=== TESTING ENDS ==================+=="
