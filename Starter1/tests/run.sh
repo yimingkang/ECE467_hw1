2e8a38854c39b5d4fbb2d8a41b28413437325db5
Ryan Kang <yimingkang@hotmail.com>
2015-10-07 02:53:58 -0400

    more testing

--- Starter1/tests/run.sh
+++ Starter1/tests/run.sh
@@ -4,13 +4,24 @@ echo ""
 for file in ./*.frog; do
     ../compiler467 -Tn "$file" > "$file".out
     DIFF=$(diff "$file".out "$file".target)
+    OUT=$(python verify.py "$file".out)
     if [ "$DIFF" != "" ] 
     then
-        echo "*****TEST $file FAILED*****"
+        echo "*****TEST $file FAILED TO MATCH TARGET*****"
+        printf "diff   %-30s --- FAILED\n" "$file"
         diff "$file".out "$file".target
     else
-        echo "OK"
+        printf "diff   %-30s --- OK\n" "$file"
+    fi
+
+    if [ "$OUT" != "" ] 
+    then
+        echo "*****TEST $file FAILED TO PRODUCE CONSITENT TOKEN*****"
+        printf "verify %-30s --- FAILED\n" "$file"
+        python verify.py "$file".out
+    else
+        printf "verify %-30s --- OK\n" "$file"
     fi
 done
 rm -f *.out
-echo "================+=== TESTING ENDS ==================+=="
+echo "==================== TESTING ENDS ====================="
--- Starter1/tests/verify.py
+++ Starter1/tests/verify.py
@@ -11,15 +11,17 @@ def main(file_name):
     lex_output_regex = re.compile(r"TOKEN\s+(\d{3})\s+:\s+(.*)")
     seen_tokens = dict()
     ret = 0
+    line_num = 0
     with open(file_name, 'r') as fin:
         for line in fin:
+            line_num += 1
             matched = lex_output_regex.match(line)
             value = matched.group(1)
             token = matched.group(2)
             if token in KNOWN_TOKENS:
                 if token in seen_tokens.keys():
                     if seen_tokens[token] != value:
-                        print "ERROR: Token ", token, " is asigned a new value!"
+                        print "ERROR: Token ", token, " is asigned a new value on line #", str(line_num)
                         ret = -1
                 else:
                     seen_tokens[token] = value
@@ -30,9 +32,6 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "ERROR: Invalid argument"
         exit(0)
-    print "***** TEST START*****"
     if main(sys.argv[1]) == -1:
         print "***** TEST FAILED *****"
-    else:
-        print "***** TEST PASSED *****"
 
